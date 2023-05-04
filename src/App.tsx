608c7fb835ae1c7f133224b62c04ccdf2d1530f4
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-05-04 22:53:23 +0700

    add searchbox and cardlist component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import CardList from "./components/CardList";
+import SearchBox from "./components/SearchBox";
 
 function App() {
   return (
     <div className="App">
       <h1>Moster Rolodex</h1>
+      <SearchBox />
+      <CardList />
     </div>
   );
 }

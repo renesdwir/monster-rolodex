7aa0248448dfa81b2e2622e2da1c6f9d232d39d7
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-05-08 21:14:23 +0700

    fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,45 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import CardList from "./components/CardList";
-import SearchBox from "./components/SearchBox";
+import CardList from "./components/CardList/CardList";
+import SearchBox from "./components/SearchBox/SearchBox";
+import { MonsterTypes } from "./types";
 
 function App() {
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonster, setFilteredMonster] = useState(monsters);
+  const [searchField, setSearchField] = useState("");
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const data = await response.json();
+        setMonsters(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const filteredMonster = monsters.filter((monster: MonsterTypes) => {
+      return monster.name
+        .toLocaleLowerCase()
+        .includes(searchField.toLocaleLowerCase());
+    });
+    setFilteredMonster(filteredMonster);
+  }, [monsters, searchField]);
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchField(e.target.value);
+  };
   return (
     <div className="App">
       <h1>Moster Rolodex</h1>
-      <SearchBox />
-      <CardList />
+      <SearchBox onChangeHandler={onChangeHandler} />
+      <CardList datas={filteredMonster} />
     </div>
   );
 }
